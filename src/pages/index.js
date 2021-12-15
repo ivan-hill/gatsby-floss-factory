8eea616032f92cb99815c4529860ffa913123511
Ivan Hill <30849044+ivan-hill@users.noreply.github.com>
2021-12-15 22:21:32 +0100

    Add files via upload

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import React from "react"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <div>
+    <SEO title="Home" />
+    <div className="banner">
+      <div className="inner-banner">
+        <div className="container">
+          <h1 className="main-headline extra large ">Floss Factory |</h1>
+          <h1 className="main-headline medium">Floss Factory</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
+export default IndexPage
